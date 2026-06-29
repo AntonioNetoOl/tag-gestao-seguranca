4cea75e65cb0a492b896f01f37b1b8a0a085a1ba
Antonio Neto <antonioolimpiodesaneto@gmail.com>
2026-06-29 11:57:23 -0300

    feat: iniciar componente de relatorio de pagamentos

---
+++ src/app/features/relatorios/relatorios-pagamentos.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relatorios-pagamentos',
+  standalone: true,
+  template: '<section></section>'
+})
+export class RelatoriosPagamentosComponent {}

afd49057cc3b59840e983ce3acadea0ec3053097
Antonio Neto <antonioolimpiodesaneto@gmail.com>
2026-06-29 11:56:13 -0300

    feat: adicionar filtros de relatorio de pagamentos

--- src/app/features/relatorios/relatorios.models.ts
+++ src/app/features/relatorios/relatorios.models.ts
@@ -5,4 +5,10 @@ export interface RelatorioEscalaFiltros {
   nomeEvento?: string;
 }
 
+export interface RelatorioPagamentoFiltros {
+  busca?: string;
+  dataInicio: string;
+  dataFim: string;
+}
+
 export type RelatorioFormato = 'excel' | 'pdf';
